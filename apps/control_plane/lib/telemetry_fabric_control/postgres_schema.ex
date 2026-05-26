318bf4cdfcd854e050ace8c89f0219edb0fa0326
Jie Zhang <2012044zj@gmail.com>
2026-05-27 00:12:11 +0800

    Fix CI action and release migration path

--- apps/control_plane/lib/telemetry_fabric_control/postgres_schema.ex
+++ apps/control_plane/lib/telemetry_fabric_control/postgres_schema.ex
@@ -6,11 +6,17 @@ defmodule TelemetryFabricControl.PostgresSchema do
   one stable place to load the first PostgreSQL migration.
   """
 
-  @migration_path Path.expand("../../priv/postgres/001_control_plane_schema.sql", __DIR__)
+  @migration_file Path.join("postgres", "001_control_plane_schema.sql")
+  @source_migration_path Path.expand("../../priv/postgres/001_control_plane_schema.sql", __DIR__)
 
-  def migration_path, do: @migration_path
+  def migration_path do
+    case :code.priv_dir(:telemetry_fabric_control) do
+      priv_dir when is_list(priv_dir) -> Path.join(List.to_string(priv_dir), @migration_file)
+      {:error, _reason} -> @source_migration_path
+    end
+  end
 
   def migration_sql do
-    File.read!(@migration_path)
+    File.read!(migration_path())
   end
 end
